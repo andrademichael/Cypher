61347ff718ec59ff5d103908aacb39f311ee4e06
ma <andrade.michael.e@gmail.com>
2016-08-08 16:53:03 -0700

    Commit it not quit it

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,8 @@
-jQuery("p").click(function() {
-  console.log(prompt("Enter some words."));
+  inputWords = localStorage.getItem(prompt("Enter sentence with more than two words."));
+jQuery("h2").click(function() {
+  var reFirst = new RegExp("/./")
+  var reLast = new RegExp("/^/")
+  var firstLetter = inputWords.match(reFirst);
+  var lastLetter = inputWords.match(reLast);
+  alert(firstLetter + lastLetter);
 });
